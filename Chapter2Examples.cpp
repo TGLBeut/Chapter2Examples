7c17f7a7588df4e2a4a08a4b603cb40ff1768989
TGLBeut <Sgre9381@gmail.com>
2025-08-24 00:11:50 -0400

    Add project files.

---
+++ Chapter2Examples.cpp
@@ -0,0 +1,37 @@
+/*Program File Name Chapter2Examples
+Programmer: Sean M Greene
+Date: August 23/2025
+Requirements:
+Write a program that has the following character variables:
+first, middle, and last. Store these initials in these variables
+then display them on the screen.
+*/
+
+#include <iostream>
+
+
+int main()
+{
+	
+	char s;
+	char m;
+	char g;
+	std::cout << "Please enter the initial of your first name:";
+	std::cin >> s;
+	std::cout << "Please enter the initial of your middle name:";
+	std::cin >> m;
+	std::cout << "Please enter the initial of your last name:";
+	std::cin >> g;
+	std::cout << "Your initals are: " << s << m << g;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
