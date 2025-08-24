b9991012a84bc07ecf699c562fed91a7dbeb114d
TGLBeut <Sgre9381@gmail.com>
2025-08-24 00:23:06 -0400

    Added code for a first name Display

--- Chapter2Examples.cpp
+++ Chapter2Examples.cpp
@@ -8,7 +8,7 @@ then display them on the screen.
 */
 
 #include <iostream>
-
+#include <string>
 
 int main()
 {
@@ -16,13 +16,17 @@ int main()
 	char s;
 	char m;
 	char g;
+	std::string firstname;
 	std::cout << "Please enter the initial of your first name:";
 	std::cin >> s;
 	std::cout << "Please enter the initial of your middle name:";
 	std::cin >> m;
 	std::cout << "Please enter the initial of your last name:";
 	std::cin >> g;
-	std::cout << "Your initals are: " << s << m << g;
+	std::cout << "Your initals are: " << s << m << g << std::endl;
+	std::cout << "Please enter your first name:";
+	std::cin >> firstname;
+	std::cout << "Your first name is : " << firstname;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
